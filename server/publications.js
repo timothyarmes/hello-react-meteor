bb7f8af3af1a93206c700667fb56c7f43fc6e100
Timothy Armes <tim@timothyarmes.com>
2015-11-30 08:53:51 +0100

    Show problem with custom publications

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,12 @@
-Meteor.publish('posts', () => {
-  const selector = {category: {$ne: "private"}};
-  return Posts.find(selector);
+Meteor.publish('posts', function() {
+  const selector = {category: {$ne: 'private'}};
+  posts = Posts.find(selector);
+
+  posts.forEach((post) => {
+    this.added('posts', post._id, post);
+  });
+
+  this.ready();
 });
 
 Meteor.publish('singlePost', _id => {
